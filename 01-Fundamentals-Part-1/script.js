823662d189d4b5c949a1661e93148aa9bae2cad2
ucangun <ucangun76@gmail.com>
2024-01-24 11:23:19 +0100

    switch statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -330,7 +330,7 @@ if ((scoreDolphins && scoreKoalas) >= 100 && scoreDolphins > scoreKoalas) {
 // NUMBER THREE //
 
 const scoreDolphins = (97 + 112 + 101) / 3;
-const scoreKoalas = (109 + 95 + 106) / 3;
+const scoreKoalas = (109 + 95 + 123) / 3;
 console.log(scoreDolphins, scoreKoalas);
 
 if ((scoreDolphins && scoreKoalas) >= 100 && scoreDolphins > scoreKoalas) {
@@ -345,5 +345,46 @@ if ((scoreDolphins && scoreKoalas) >= 100 && scoreDolphins > scoreKoalas) {
   scoreDolphins === scoreKoalas
 ) {
   console.log("DRAW!");
+} else {
+  console.log("No one wins the trophy");
 }
 */
+
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory videos");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record video");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Enjoy the weekend");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+
+if (day === "monday") {
+  console.log(`Plan course structure \n\Go to coding meetup`);
+} else if (day === "tuesday") {
+  console.log("Prepare theory videos");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record video");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Enjoy the weekend");
+} else {
+  console.log("Not a valid day");
+}
