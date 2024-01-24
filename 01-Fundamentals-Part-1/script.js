72b06156f325f6d58499f64bea26297c8334b3f0
ucangun <ucangun76@gmail.com>
2024-01-24 11:51:40 +0100

    Conditional Operator

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -350,6 +350,7 @@ if ((scoreDolphins && scoreKoalas) >= 100 && scoreDolphins > scoreKoalas) {
 }
 */
 
+/*
 const day = "monday";
 
 switch (day) {
@@ -388,3 +389,24 @@ if (day === "monday") {
 } else {
   console.log("Not a valid day");
 }
+*/
+
+/*
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
+*/
