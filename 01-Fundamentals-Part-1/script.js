7a0865e696e3e276aed99db7a93ad7ce930bb487
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-02 21:45:37 +0200

    Challenge 1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -104,6 +104,20 @@ console.log(x, y);
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
 */
+
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+
+console.log(BMIJohn, BMIMark);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+
 /*
 //const massMark = 78;
 //const heightMark = 1.69;
