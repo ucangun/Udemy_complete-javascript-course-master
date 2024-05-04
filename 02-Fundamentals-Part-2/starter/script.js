a5232b999fa59f73facdf9eec8dc82f429e9d317
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-05 00:46:01 +0200

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,41 +10,107 @@ if (hasDriversLicence) console.log("I can drive :D");
 //const interFace="Audio"
 //const private="534"
 */
+/*
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+//console.log(fruitProcessor(5, 0)); // directly without a variable
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
 
 /*
-// Versuch 1
+// !Function Decleration
+/////////////////
 function calcAge1(birthYear) {
-  const age = 2037 - birthYear;
+  const age = 2024 - birthYear;
   return age;
 }
+
 const ageUmut = calcAge1(1998);
 console.log(ageUmut);
 
-//Versuch 2
-
+///////////////////
 function calcAge10(birthYear) {
-  return 2037 - birthYear;
+  return 2024 - birthYear;
 }
 
-const ageBerra = calcAge10(2000);
-console.log(ageBerra);
+const ageFatos = calcAge10(1975);
+console.log(ageFatos);
 
-//Versuch 3
+// ! Function Expression
 
 const calcAge100 = function (birthYear) {
-  return 2037 - birthYear;
+  return 2024 - birthYear;
 };
 
-const ageBerfin = calcAge100(2001);
+const ageBerra = calcAge100(2000);
+console.log(ageBerra);
+*/
+/*
+// ! Arrow Function
+const calcAge1000 = (birthYear) => 2024 - birthYear;
+
+const ageBerfin = calcAge1000(2000);
 console.log(ageBerfin);
 
-//Versuch 4
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2024 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstName} retires in ${retirement} years!`;
+};
 
-const calcAge1000 = (birthYear) => 2037 - birthYear;
-const ageFatos = calcAge1000(1975);
-console.log(ageFatos);
+const retirementUmut = yearsUntilRetirement(1998, "Umut");
+console.log(retirementUmut);
 */
 
+/*
+// ! Calling another Function
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 4));
+*/
+
+/*
+// ! Review
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years!`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired`);
+    return -1;
+  }
+  //return retirement;
+};
+
+const retirementUmut = yearsUntilRetirement(1998, "Umut");
+console.log(retirementUmut);
+*/
 /*
 function logger() {
   console.log("Hey! My name is Umut...");
@@ -113,7 +179,7 @@ const yearUntilRetirement = (birthYear, firstName) => {
 console.log(yearUntilRetirement(1998, "Umut"));
 
 */
-
+/*
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
