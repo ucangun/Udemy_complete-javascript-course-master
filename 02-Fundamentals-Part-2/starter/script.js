926385303b54fa628b89d0403a71ff91aad08fcb
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-05 23:14:46 +0200

    Challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -344,6 +344,7 @@ const ages = [
 console.log(ages);
 */
 
+/*
 const friends = ["Micheal", "Steven", "Peter"];
 
 //Add elements
@@ -376,3 +377,29 @@ console.log(friends.includes(23));
 if (friends.includes("Steven")) {
   console.log("You have a friend called Steven");
 }
+*/
+
+// ! CHALLENGE 2
+
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills, tips);
+
+const total = [
+  bills[0] + calcTip(bills[0]),
+  bills[1] + calcTip(bills[1]),
+  bills[2] + calcTip(bills[2]),
+];
+console.log(total);
+
+//console.log(calcTip(100));
