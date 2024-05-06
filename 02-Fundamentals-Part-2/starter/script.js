d096ec96cb28cd5c6ecdd2bc5797efac224c5a24
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-06 14:06:59 +0200

    for loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -516,6 +516,7 @@ console.log(umut.getSummary());
 
 // ! CHALLENGE 3
 
+/*
 const Mark = {
   fullName: "Mark Miller",
   mass: 78,
@@ -544,3 +545,25 @@ console.log(
     ? `${John.fullName}'s ${John.BMI} BMI is higher than ${Mark.fullName}'s ${Mark.BMI} BMI `
     : `${Mark.fullName}'s ${Mark.BMI} BMI is higher than ${John.fullName}'s ${John.BMI} BMI`
 );
+*/
+
+/* ---------------------------------- */
+/*              // ! LOOP             */
+/* ---------------------------------- */
+/*
+console.log("Lifting weights repetition 1");
+console.log("Lifting weights repetition 2");
+console.log("Lifting weights repetition 3");
+console.log("Lifting weights repetition 4");
+console.log("Lifting weights repetition 5");
+console.log("Lifting weights repetition 6");
+console.log("Lifting weights repetition 7");
+console.log("Lifting weights repetition 8");
+console.log("Lifting weights repetition 9");
+console.log("Lifting weights repetition 10");
+*/
+// for loop keeps runnung while condition is TRUE
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋🏻‍♂️ `);
+}
