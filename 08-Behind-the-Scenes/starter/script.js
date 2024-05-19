453d8aaed1b4cf7a782b58816265c86a20a77d21
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-19 10:52:24 +0200

    Hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -30,3 +31,48 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+*/
+
+// ! Variables
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1998;
+
+// ! Functions
+
+console.log(addDecl(5, 3));
+//console.log(addExpr(5, 3));
+//console.log(addArrow(5, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// ! Example
+
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// !
+
+var x = 2;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(x === window.y);
+console.log(x === window.z);
