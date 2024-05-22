53b201c7a46022573f80f5f837c14815f393366f
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-23 00:30:39 +0200

    Rest pattern and Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -50,8 +50,58 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3} `
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+/////////////////////////////////////////////////
+//***********************************************/
+// 1) Destructuring
+
+// SPREAD , because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST , because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza, risotto, otherFood);
+
+// Objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
 };
 
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+//////////////////////////////////////////////////
+//************************************************/
+/*
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -80,11 +130,11 @@ console.log(...str);
 
 // Real-World Example
 const ingredients = [
-  /*
+  
   prompt("Let's make pasta! Ingredient 1?"),
   prompt('Ingredient 2?'),
   prompt('Ingredient 3'),
-  */
+  
 ];
 console.log(ingredients);
 
@@ -100,7 +150,7 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.nameRestaurant);
-
+*/
 //////////////////////////////////////
 //***********************************/
 /*
