4fd65c2ed88f828a9c9e015deda3674ad3fc7fda
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-23 21:22:52 +0200

    Short Circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -58,6 +58,36 @@ const restaurant = {
 };
 /////////////////////////////////////////////////
 //***********************************************/
+
+//!   SHORT-CIRCUITING
+console.log('-------OR---------');
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('-------AND---------');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('Hello' && 12 && null && 'Jonas');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+/////////////////////////////////////////////////
+//***********************************************/
+/*
 // 1) Destructuring
 
 // SPREAD , because on RIGHT side of =
@@ -98,7 +128,7 @@ add(...x);
 
 restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
 restaurant.orderPizza('mushrooms');
-
+*/
 //////////////////////////////////////////////////
 //************************************************/
 /*
--- Assignments/app.js
+++ Assignments/app.js
@@ -309,3 +309,17 @@ function printBookAuthorsCount(title, ...authors) {
 }
 
 printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
+
+// !
+
+function hasExamplesInJava(book) {
+  return book.programmingLanguage == "Java" || "no data available";
+}
+
+console.log(hasExamplesInJava(books[0]));
+console.log(hasExamplesInJava(books[1]));
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent &&
+    console.log(`"${books[i].title}" provides online content`);
+}
