3deb4f33908e29bdebf81bc261b844418270bd41
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-24 16:10:56 +0200

    Challenge 1

--- Assignments/app.js
+++ Assignments/app.js
@@ -323,8 +323,81 @@ for (let i = 0; i < books.length; i++) {
   books[i].onlineContent &&
     console.log(`"${books[i].title}" provides online content`);
 }
-
+/*
 for (let i = 0; i <= books.length; i++) {
   books[i].onlineContent ??
     console.log(`"${title}" provides no data about its online content`);
 }
+*/
+console.log(
+  "*******************************************************************************************************************************"
+);
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//1.
+const [players1, players2] = game.players;
+//2.
+const [gk, ...fieldPlayers1] = players1;
+console.log(fieldPlayers1);
+//3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+//4.
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+//5.
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+//6.
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals("Davies", "Muller");
+printGoals(...game.scored);
+
+//7.
+team1 < team2 && console.log("Team 1 is more likely to win ");
+team1 > team2 && console.log("Team 2 is more likely to win ");
