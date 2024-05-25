4e3a4d60f88e5582d9b3370f87db411666b6f8ed
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-25 16:57:50 +0200

    Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -59,6 +59,77 @@ const restaurant = {
 /////////////////////////////////////////////////
 //***********************************************/
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon,Portugal');
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+//rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
+
+//****/
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎉'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//
+
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+//const answer = +prompt('Your answer ');
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+// Convert Map to array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+/////////////////////////////////////////////////
+//***********************************************/
+/*
 const ordersSet = new Set([
   'Pasta',
   'Pizza',
@@ -91,7 +162,7 @@ console.log(
 console.log(new Set('jonasschmedtman').size);
 
 console.log(new Set('Jonas'));
-
+*/
 /////////////////////////////////////////////////
 //***********************************************/
 /*
