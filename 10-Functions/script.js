8f8c0a01986a55e16719de44accb38c7013e1ce4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-27 17:20:23 +0200

    Functions accepting Callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -26,6 +26,7 @@ createBooking("LH123", 5);
 createBooking("LH123", undefined, 1000);
 */
 
+/*
 const flight = "LH234";
 const jonas = {
   name: "Jonas Schmedtmann",
@@ -53,3 +54,32 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher Order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log("👋");
+};
+document.body.addEventListener("click", high5);
+
+["Jonas", "Martha", "Adam"].forEach(high5);
