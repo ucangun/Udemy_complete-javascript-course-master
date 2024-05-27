9b4050602a92452d7508f8d476733bfbc2c8b514
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-28 01:14:31 +0200

    The Bind Method

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -153,3 +153,47 @@ console.log(swiss);
 
 // Modern way
 book.call(swiss, ...flightData);
+
+// Bind Method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, "Steven");
+
+//!
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Berra");
+bookEW23("Berfin");
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+//lufthansa.buyPlane();
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
