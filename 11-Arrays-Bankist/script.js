49f2f8b4c5c5881e21c0af486f065993717fed18
ucangun <149247682+ucangun@users.noreply.github.com>
2024-05-30 14:56:59 +0200

    The new at method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -75,6 +75,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
 let arr = ["a", "b", "c", "d", "e"];
 
 // SLICE
@@ -113,3 +114,17 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(" - "));
+*/
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+
+console.log("jonas".at(0));
+console.log("jonas".at(-1));
