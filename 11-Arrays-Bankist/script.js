8dc712a886f4d0d765894145fc43b5e9df21ec73
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-01 23:43:26 +0200

    reduce method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -79,6 +79,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach((acc) => {
     acc.username = acc.owner
@@ -236,3 +242,20 @@ console.log(depositsFor);
 const withdrawals = movements.filter((a) => a < 0);
 console.log(withdrawals);
 */
+
+// accumulator ---> SNOWBALL
+
+console.log(movements);
+const balance = movements.reduce((acc, curr) => acc + curr, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
