47f42dd9d2a2d699f0ea9ab3f25a7ceb18d72817
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-23 15:46:31 +0200

    Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -25,3 +25,29 @@ const jack = new Person("Jack", 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person); // True
+
+///////////////////////////////////////
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype); // True
+
+console.log(Person.prototype.isPrototypeOf(jonas)); // True
+console.log(Person.prototype.isPrototypeOf(matilda)); // True
+console.log(Person.prototype.isPrototypeOf(Person)); // False
+
+// .prototyeOfLinkedObjects
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("species"));
