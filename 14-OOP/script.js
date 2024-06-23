0a5593bb7647f02c5835d9d83452f8eee7866e55
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-23 16:20:02 +0200

    Challenge 1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,4 +1,6 @@
 "use strict";
+
+/*
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -25,9 +27,11 @@ const jack = new Person("Jack", 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person); // True
-
+*/
 ///////////////////////////////////////
 // Prototypes
+
+/*
 console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
@@ -51,9 +55,10 @@ console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty("firstName"));
 console.log(jonas.hasOwnProperty("species"));
-
+*/
 ///////////////////////////////////////
 // Prototypal Inheritance on Built-In Objects
+/*
 console.log(jonas.__proto__);
 // Object.prototype (top of prototype chain)
 console.log(jonas.__proto__.__proto__);
@@ -75,3 +80,42 @@ console.log(arr.unique());
 
 const h1 = document.querySelector("h1");
 console.dir((x) => x + 1);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car("BMW", 120);
+const mercedes = new Car("Mercedes", 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
