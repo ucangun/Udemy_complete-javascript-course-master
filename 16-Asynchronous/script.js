c0bc099be4014e1bd4f7b87387c6882ed2c4d9c4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-06-25 00:53:54 +0200

    The event loop in practice

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -210,6 +210,7 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then((res) => {
@@ -217,7 +218,7 @@ const whereAmI = function (lat, lng) {
       return res.json();
     })
     .then((data) => {
-      console.log(data);
+      //console.log(data);
       console.log(`You are in ${data.city}, ${data.country}`);
 
       return fetch(
@@ -235,3 +236,18 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+//////////////////////////////////////
+
+console.log("Test start");
+setTimeout(() => {
+  console.log("0 sec timer"), 0;
+});
+Promise.resolve("Resolved promise 1 ").then((res) => console.log(res));
+Promise.resolve("Resolved promise 2 ").then((res) => {
+  for (let i = 0; i < 1000; i++) {}
+  console.log(res);
+});
+
+console.log("Test end");
